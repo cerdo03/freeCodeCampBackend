c6fb2784a54e6a59d171004cc8be4d6ebe1bae87
cerdo03 <surya@uthaan.co.in>
2023-06-17 10:13:36 +0530

    one more task

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -6,6 +6,14 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+"/views/index.html");
 });
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/json',function(req,res){
+    const data = {
+        message:"Hello json"
+    }
+    res.json(data);
+});
 
 
 
