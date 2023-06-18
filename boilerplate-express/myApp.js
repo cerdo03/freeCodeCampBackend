c32eab3c389d1a1314748d803746b45004e20186
cerdo03 <surya@uthaan.co.in>
2023-06-18 16:19:20 +0530

    middleware

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 // let __dirname="/Users/suryashukla/Documents/Web Dev/freeCodeCampBackend"
@@ -6,15 +7,28 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+"/views/index.html");
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/json',function(req,res){
-    const data = {
-        message:"Hello json"
-    }
-    res.json(data);
-});
+// app.get('/json',function(req,res){
+//     let data = {
+//         message:"Hello json"
+//     }
+//     if(process.env.MESSAGE_STYLE=="uppercase"){
+//         data.message=data.message.toUpperCase();
+//     }
+//     res.json(data);
+// });
 
+function logger(req,res,next){
+    const method=req.method;
+    const path = req.path;
+    const ip = req.ip;
+
+    console.log(`${method} ${path} - ${ip}`);
+    next();
+}
+
+app.use(logger);
 
 
 
