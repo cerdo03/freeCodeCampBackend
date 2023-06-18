11be91272d34df773bfc5e21008d9b40d9b19f39
cerdo03 <surya@uthaan.co.in>
2023-06-18 16:31:07 +0530

    middle ware current time

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -3,6 +3,15 @@ let express = require('express');
 let app = express();
 // let __dirname="/Users/suryashukla/Documents/Web Dev/freeCodeCampBackend"
 
+app.get('/now',function(req,res,next){
+    req.time=new Date().toString();
+    next();
+},function(req,res){
+    res.send({time:req.time});
+   
+});
+// app.use(logger);
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/views/index.html");
 });
@@ -19,16 +28,14 @@ app.get('/',function(req,res){
 //     res.json(data);
 // });
 
-function logger(req,res,next){
-    const method=req.method;
-    const path = req.path;
-    const ip = req.ip;
+// function logger(req,res,next){
+//     const method=req.method;
+//     const path = req.path;
+//     const ip = req.ip;
 
-    console.log(`${method} ${path} - ${ip}`);
-    next();
-}
-
-app.use(logger);
+//     console.log(`${method} ${path} - ${ip}`);
+//     next();
+// }
 
 
 
